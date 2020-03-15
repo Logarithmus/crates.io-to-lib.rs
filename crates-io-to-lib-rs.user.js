2897f6b3ab254310f637b2bd237b14497821e705
Logarithmus <logarithmus.dev@gmail.com>
2020-03-15 15:53:08 +0300

    Fixed license

--- crates-io-to-lib-rs.user.js
+++ crates-io-to-lib-rs.user.js
@@ -1,8 +1,9 @@
 // ==UserScript==
 // @name         crates.io -> lib.rs
+
 // @version      0.1.0
-// @license The Unlicense; https://unlicense.org/
-// @contributionURL https://github.com/Logarithmus/crates-io-to-lib-rs
+// @license Unlicense; https://unlicense.org/
+// @homepageURL https://github.com/Logarithmus/crates-io-to-lib-rs
 // @description  Replace all links to crates.io with ones to lib.rs, as the latter is much faster cause it doesn't use tons of javascript
 // @author       Logarithmus
 // @match        *://*/*
