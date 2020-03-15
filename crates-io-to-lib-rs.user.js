d9413e4dac194bab52206957999f0cf23c24129f
Logarithmus <logarithmus.dev@gmail.com>
2020-03-15 16:15:26 +0300

    Fixed false-positive when crates.io isn't a domain name

--- crates-io-to-lib-rs.user.js
+++ crates-io-to-lib-rs.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         crates.io -> lib.rs
-// @version      0.1.0
+// @version      0.1.1
 // @license      GPL-3.0-only; http://www.gnu.org/licenses/gpl-3.0.txt
 // @homepageURL  https://github.com/Logarithmus/crates-io-to-lib-rs
 // @description  Replace all links to crates.io with ones to lib.rs, as the latter is much faster cause it doesn't use tons of javascript
@@ -9,7 +9,7 @@
 // ==/UserScript==
 
 (function() {
-    const regex = /(.*?)crates\.io(.*)/
+    const regex = /(.*?\:\/\/)crates\.io(.*)/
     for (let a of document.getElementsByTagName("a")) {
         a.href = a.href.replace(regex, "$1lib.rs$2")
     }
