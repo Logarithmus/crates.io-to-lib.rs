102416a59f71e0d8262674250ad6ca89748b7a92
Logarithmus <logarithmus.dev@gmail.com>
2020-03-16 12:28:46 +0300

    Bump version to 0.1.2

--- crates-io-to-lib-rs.user.js
+++ crates-io-to-lib-rs.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         crates.io -> lib.rs
-// @version      0.1.1
+// @version      0.1.2
 // @license      GPL-3.0-only; http://www.gnu.org/licenses/gpl-3.0.txt
 // @homepageURL  https://github.com/Logarithmus/crates-io-to-lib-rs
 // @description  Replace all links to crates.io with ones to lib.rs, as the latter is much faster cause it doesn't use tons of javascript
