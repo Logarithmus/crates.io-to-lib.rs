5ac0bc3950427aba0e20c9a1874426459c4fcc28
Logarithmus <logarithmus.dev@gmail.com>
2020-03-18 01:11:12 +0300

    0.1.5: Optimized window.location check - moved out of the loop

--- crates-io-to-lib-rs.user.js
+++ crates-io-to-lib-rs.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         crates.io -> lib.rs
-// @version      0.1.4
+// @version      0.1.5
 // @license      GPL-3.0-only; http://www.gnu.org/licenses/gpl-3.0.txt
 // @homepageURL  https://github.com/Logarithmus/crates-io-to-lib-rs
 // @description  Replace all links to crates.io with ones to lib.rs, as the latter is much faster because it doesn't use tons of JavaScript
@@ -9,11 +9,14 @@
 // ==/UserScript==
 
 (function() {
+    if (window.location.hostname === "crates.io") {
+        return
+    }
+
     for (let a of document.getElementsByTagName("a")) {
         // Only modify relevant <a> tags to avoid breaking sites
         if (a.hostname === "crates.io"
-         && a.pathname.indexOf("/crates/") === 0
-         && window.location.hostname != "crates.io") {
+         && a.pathname.indexOf("/crates/") === 0) {
             a.hostname = "lib.rs"
         }
     }
