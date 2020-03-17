d89f364550454f5e9c44f532e183f9a77ac18e58
Logarithmus <logarithmus.dev@gmail.com>
2020-03-17 23:46:30 +0300

    0.1.4: now this script only affects links to the crates, also it doesn't touch the links located on crates.io itself

--- crates-io-to-lib-rs.user.js
+++ crates-io-to-lib-rs.user.js
@@ -1,18 +1,20 @@
 // ==UserScript==
 // @name         crates.io -> lib.rs
-// @version      0.1.2
+// @version      0.1.4
 // @license      GPL-3.0-only; http://www.gnu.org/licenses/gpl-3.0.txt
 // @homepageURL  https://github.com/Logarithmus/crates-io-to-lib-rs
-// @description  Replace all links to crates.io with ones to lib.rs, as the latter is much faster cause it doesn't use tons of javascript
+// @description  Replace all links to crates.io with ones to lib.rs, as the latter is much faster because it doesn't use tons of JavaScript
 // @author       Logarithmus
 // @match        *://*/*
 // ==/UserScript==
 
 (function() {
-    const regex = /(.*?\:\/\/)crates\.io(.*)/
     for (let a of document.getElementsByTagName("a")) {
-        if (a.hasAttribute("href")) {
-        	a.href = a.href.replace(regex, "$1lib.rs$2")
+        // Only modify relevant <a> tags to avoid breaking sites
+        if (a.hostname === "crates.io"
+         && a.pathname.indexOf("/crates/") === 0
+         && window.location.hostname != "crates.io") {
+            a.hostname = "lib.rs"
         }
     }
 })();
