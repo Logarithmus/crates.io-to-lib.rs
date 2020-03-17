0d8d5064a1ba8ce987bc06972f15e747aa90c781
Stephan Sokolow <http://www.ssokolow.com/ContactMe>
2020-03-17 10:04:03 -0400

    Switch to a more robust approach to handling unexpected inputs

--- crates-io-to-lib-rs.user.js
+++ crates-io-to-lib-rs.user.js
@@ -1,18 +1,28 @@
 // ==UserScript==
 // @name         crates.io -> lib.rs
-// @version      0.1.2
+// @version      0.1.3
 // @license      GPL-3.0-only; http://www.gnu.org/licenses/gpl-3.0.txt
 // @homepageURL  https://github.com/Logarithmus/crates-io-to-lib-rs
-// @description  Replace all links to crates.io with ones to lib.rs, as the latter is much faster cause it doesn't use tons of javascript
+// @description  Replace all links to crates.io with ones to lib.rs, as the latter is much faster because it doesn't use tons of JavaScript
 // @author       Logarithmus
 // @match        *://*/*
 // ==/UserScript==
 
 (function() {
-    const regex = /(.*?\:\/\/)crates\.io(.*)/
+    let url = null;
     for (let a of document.getElementsByTagName("a")) {
-        if (a.hasAttribute("href")) {
-        	a.href = a.href.replace(regex, "$1lib.rs$2")
+        // Skip to next tag on "not a valid URL"
+        try {
+            // Rely on the browser's own URL parser for robustness
+            url = new URL(a.href);
+        } catch (e) {
+            continue;
+        }
+
+        // Only modify relevant <a> tags to avoid breaking sites
+        if (url.hostname === "crates.io") {
+          url.hostname = "lib.rs";
+          a.href = url.toString();
         }
     }
 })();
