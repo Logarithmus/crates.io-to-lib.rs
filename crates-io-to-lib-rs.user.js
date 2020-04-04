295eac56ae5a49fc5d0e81eea347506387df6bed
Logarithmus <logarithmus.dev@gmail.com>
2020-04-04 22:19:41 +0300

    Added @updateURL, @downloadURL (to prevent counting script updates as new installs) and @copyright sections to UserScript Block

--- crates-io-to-lib-rs.user.js
+++ crates-io-to-lib-rs.user.js
@@ -3,6 +3,9 @@
 // @version      0.1.5
 // @license      GPL-3.0-only; http://www.gnu.org/licenses/gpl-3.0.txt
 // @homepageURL  https://github.com/Logarithmus/crates-io-to-lib-rs
+// @updateURL    https://openuserjs.org/meta/Logarithmus/crates.io_-_lib.rs.meta.js
+// @downloadURL  https://openuserjs.org/install/Logarithmus/crates.io_-_lib.rs.user.js
+// @copyright    2020, Logarithmus (https://openuserjs.org/users/Logarithmus)
 // @description  Replace all links to crates.io with ones to lib.rs, as the latter is much faster because it doesn't use tons of JavaScript
 // @author       Logarithmus
 // @match        *://*/*
